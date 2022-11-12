000e34b6fb71387dad0dc003d21f0df18be8aa6a
tetheresemarie <te.theresemarie888@gmail.com>
2022-11-12 14:52:49 +0800

    Modified DayGreet.html

--- TodoList.js
+++ TodoList.js
@@ -1,17 +1,2 @@
-let addToDoButton = document.getElementById('addToDo');
-let ToDoContainer = document.getElementById('ToDoContainer');
-let inputField = document.getElementById('InputField');
-
-addToDoButton.addEventListener('click', function (){
-var para = document.createElement ('p')
-para.classList.add('para')
-para.innerText = inputField.value;
-ToDoContainer.appendChild(para); 
-inputField.value = "";
-para.addEventListener ('click', function(){
-     para.style.textDecoration = "line-through";
-})
-para.addEventListener ('dblclick', function(){
-     para.ToDoContainer.removechild(para);
-})
-})
+let newTodo = document.querySelector('.todo_name');
+let addTodo = document.querySelector('.todo_name');
